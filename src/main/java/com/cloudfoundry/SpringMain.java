6d95f48088b1b4a0342c4e1835296e57b7d3f648
holy12345 <spring_holy@163.com>
2018-07-30 15:40:15 +0800

    update

--- src/main/java/com/cloudfoundry/SpringMain.java
+++ src/main/java/com/cloudfoundry/SpringMain.java
@@ -2,6 +2,8 @@ package com.cloudfoundry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 public class SpringMain {
@@ -9,4 +11,5 @@ public class SpringMain {
     public static void main(String[] args) {
         SpringApplication.run(SpringMain.class, args);
     }
+
 }
---
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Index</title>
+</head>
+<body>
+<p>Spring MVC based on XML config success!</p>
+</body>
+</html>
