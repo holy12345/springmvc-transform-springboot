b39d24c907795a0331effe41d4c4f048ec7f2235
holy12345 <spring_holy@163.com>
2018-07-30 15:54:53 +0800

    modify jar to war

--- src/main/java/com/cloudfoundry/SpringMain.java
+++ src/main/java/com/cloudfoundry/SpringMain.java
@@ -2,8 +2,6 @@ package com.cloudfoundry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 public class SpringMain {
@@ -11,5 +9,4 @@ public class SpringMain {
     public static void main(String[] args) {
         SpringApplication.run(SpringMain.class, args);
     }
-
 }
