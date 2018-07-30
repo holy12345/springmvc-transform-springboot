73939057be0a0ad08d11a0c312c5ceceb5d66fa7
holy12345 <spring_holy@163.com>
2018-07-30 14:28:31 +0800

    update

--- src/main/java/com/cloudfoundry/controller/TestController.java
+++ src/main/java/com/cloudfoundry/controller/TestController.java
@@ -2,15 +2,13 @@ package com.cloudfoundry.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
 
     @RequestMapping(value = "/cloudfoundry")
-    @ResponseBody
     public String cloudFoundry() {
-        return "Hello cloudFoundry...";
+        return "index";
     }
 
 }
